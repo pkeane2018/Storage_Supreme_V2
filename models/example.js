59033d695d71077cdb33471c6258af3c9e9484e6
test <test@stp-er-10-129-115-175.wireless.umn.edu>
2019-01-14 21:31:17 -0600

    create db info

--- models/example.js
+++ models/example.js
@@ -1,7 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var PurchaseOrders = sequelize.define("purchase_orders", {
+    projectName: DataTypes.STRING,
+    requestorname: DataTypes.STRING,
+    materialDescription: DataTypes.STRING,
+    quantity: DataTypes.INTEGER,
+    needByDate: DataTypes.DATEONLY,
+    dateSubmitted: DataTypes.DATEONLY,
+    dateOrdered: DataTypes.DATEONLY,
+    confirmedDeliveryDate: DataTypes.DATEONLY,
+    requestNumber: DataTypes.INTEGER,
+    buyer: DataTypes.STRING,
+    status: DataTypes.STRING,
+    deliveryStatus: DataTypes.STRING,
+    deliveryDateDelta: DataTypes.INTEGER,
+    createdAt: DataTypes.STRING,
+    updatedAt: DataTypes.STRING
   });
-  return Example;
+  return PurchaseOrders;
 };
