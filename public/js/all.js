b24c1aa164266542e4c411bc8c60f1d36aa138be
Patrick Keane <pkeane92@yahoo.com>
2019-01-16 19:45:21 -0600

    forgot to branch

--- public/js/all.js
+++ public/js/all.js
@@ -8,20 +8,3 @@ $(document).ready(function() {
 
 });
 
-<script>
-
-$(document).ready(function() {
-
-    $(".submit").click(function(event){
-
-      event.preventDefault();
-
-      var stuff = ("#form1").elements["assigned-buyer"].val()
-      {{!-- var things = stuff.elements["assigned-buyer"].val(); --}}
-      console.log($(this).parent().attr("id"));
-      console.log(stuff);
-    })
-
-});
-
-</script>
