9b41218b44924f0675254e5e0126119df1f4b10b
Patrick Keane <pkeane92@yahoo.com>
2019-01-17 10:18:27 -0600

    worked on js logic in procurement all handlebars file

--- public/js/all.js
+++ public/js/all.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
 
-    $(".submit").click(function(){
+  $(".submit").click(function(event){
 
-      var test = $(this).parent().elements.namedItem("assigned-buyer").val();
-      console.log(test);
-    })
+    event.preventDefault();
+
+    var update = {
+
+      buyer: $(this).parent()[0].elements[1].value,
+      material: $(this).parent()[0].elements[2].value,
+      quantity: $(this).parent()[0].elements[3].value,
+
+    };
+
+  console.log(update);
+
+  });
+
+  $.ajax({
+    method: "PUT",
+    url: "/api/procurement/all",
+    data: update
+  });
 
 });
 
