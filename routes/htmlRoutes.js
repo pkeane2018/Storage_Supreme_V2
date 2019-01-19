0d37efd2771fa7b13f43d7d6e727b7c034762e60
Jenny Boerner <jennyboerner1@gmail.com>
2019-01-19 10:09:25 -0600

    Upload CSS

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -19,70 +19,21 @@ module.exports = function(app) {
   });
 
   app.get("/procurement", function(req, res) {
-
- 
-
     db.purchase_orders.findAll({}).then(function() {
-      console.log(res);
-      res.render("procurement");
+      // console.log(res);
+      res.render("procurementall");
     });
   });
 
   app.get("/review", function(req, res) {
-    db.purchase_orders.findAll({
-      where: {
-        projectName: req.params.projectName
-      }
-    }).then(function () {
-      console.log(req);
+    db.PurchaseOrders.findAll({}).then(function() {
+      console.log(res);
       res.render("review");
     });
-
   });
-  
-
-  
-
-  // app.get("/procurement/assigned", function (req, res) {
-  //   // Info needs to be added here to display the databse
-
-  //   res.render("procassigned");
-
-  // });
-
-  // app.get("/procurement/all", function (req, res) {
-  //   // Info needs to be added here to display the databse
-
-  //   db.purchase_orders.findAll().then(function (results) {
-
-  //     res.render("procurementall", {
-  //       orders: results
-  //     });
-  //   });
-  // });
-
-
-
-  // app.get("/", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.render("index", {
-  //       // msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
-
-  // // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
